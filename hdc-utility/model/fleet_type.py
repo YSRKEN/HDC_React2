d96507989dc5957c76e909b42296665c392c0851
YSRKEN <ysr.ken@gmail.com>
2019-06-02 03:36:43 +0900

    [add] 最終攻撃力を算出するコードの進捗を進めた

--- hdc-utility/model/fleet_type.py
+++ hdc-utility/model/fleet_type.py
@@ -2,17 +2,17 @@ from enum import Enum
 
 
 class FleetType(Enum):
-    NONE = 0
-    DD = 1
-    SS = 2
-    CL = 3
-    CLT = 4
-    CA = 5
-    CAV = 6
-    AV = 7
-    CVL = 8
-    CV = 9
-    BB = 10
-    FBB = 11
-    BBV = 12
-    AP = 13
+    NONE = 0  # 不明
+    DD = 1  # 駆逐艦
+    SS = 2  # 潜水艦
+    CL = 3  # 軽巡洋艦
+    CLT = 4  # 重雷装巡洋艦
+    CA = 5  # 重巡洋艦
+    CAV = 6  # 航空巡洋艦
+    AV = 7  # 水上機母艦
+    CVL = 8  # 軽空母
+    CV = 9  # 正規空母
+    BB = 10  # 戦艦
+    FBB = 11  # 高速戦艦
+    BBV = 12  # 航空戦艦・陸上型
+    AP = 13  # 輸送艦
--- hdc-utility/service/calc_final_attack.py
+++ hdc-utility/service/calc_final_attack.py
@@ -1,7 +1,102 @@
-from typing import Dict
+from typing import Dict, List
 
+from model.Formation import Formation
+from model.attack_type import AttackType
+from model.engagement import Engagement
 from model.fleet import Fleet
+from model.fleet_type import FleetType
 from model.weapon import Weapon
+
+
+CL2_STR = {True: 'CL2', False: 'CL1'}
+
+FORMATION_STR = {
+    Formation.LINE_AHEAD: '単縦',
+    Formation.DOUBLE_LINE: '複縦',
+    Formation.DIAMOND: '輪形',
+    Formation.ECHELON: '梯形',
+    Formation.LINE_ABREAST: '単横',
+    Formation.FORMATION_3: '第三'
+}
+
+ENGAGEMENT_STR = {
+    Engagement.CROSSING_T_AD: '丁有',
+    Engagement.PARALLEL: '同航',
+    Engagement.HEAD_ON: '反航',
+    Engagement.CROSSING_T_DIS: '丁不'
+}
+
+ATTACK_TYPE_STR = {
+    AttackType.SHELL_ATTACK: '砲撃',
+    AttackType.TORPEDO_ATTACK: '雷撃',
+    AttackType.AIR_STRIKE: '航空',
+    AttackType.ANTI_SUB_ATTACK: '対潜',
+    AttackType.NIGHT_ATTACK: '夜戦'
+}
+
+def find_attack_type(fleet_type: FleetType) -> List[AttackType]:
+    """艦種から攻撃タイプを調べる
+
+    Parameters
+    ----------
+    fleet_type: FleetType
+        艦種
+
+    Returns
+    -------
+        可能な攻撃タイプ一覧
+    """
+
+    if fleet_type == FleetType.NONE:
+        return []
+
+    attack_type_list: List[AttackType] = []
+
+    # 砲撃戦(対水上)
+    if fleet_type != FleetType.SS:
+        attack_type_list.append(AttackType.SHELL_ATTACK)
+    # 雷撃戦
+    if fleet_type not in [FleetType.CVL, FleetType.CV, FleetType.BB, FleetType.FBB,
+                          FleetType.BBV, FleetType.AP]:
+        attack_type_list.append(AttackType.TORPEDO_ATTACK)
+    # 開幕航空戦
+    if fleet_type in [FleetType.CAV, FleetType.AV, FleetType.CVL, FleetType.CV, FleetType.BBV]:
+        attack_type_list.append(AttackType.AIR_STRIKE)
+    # 対潜攻撃
+    if fleet_type in [FleetType.DD, FleetType.CL, FleetType.CLT, FleetType.CAV,
+                      FleetType.AV, FleetType.CVL, FleetType.BBV]:
+        attack_type_list.append(AttackType.ANTI_SUB_ATTACK)
+    # 夜戦
+    attack_type_list.append(AttackType.NIGHT_ATTACK)
+    return attack_type_list
+
+
+def calc_final_attack_impl(fleet: Fleet, weapon_dict: Dict[id, Weapon],
+                           cl2_flg: bool, formation: Formation,
+                           engagement: Engagement, attack_type: AttackType) -> int:
+    """最終攻撃力を算出する
+
+    Parameters
+    ----------
+    fleet: Fleet
+        艦船データ
+    weapon_dict: Dict[id, Weapon]
+        装備データ一覧
+    cl2_flg: bool
+        CL2ダメージならTrue
+    formation: Formation
+        陣形
+    engagement: Engagement
+        交戦形態
+    attack_type: AttackType
+        攻撃種別
+
+    Returns
+    -------
+        最終攻撃力
+    """
+
+    return 0
 
 
 def calc_final_attack(fleet: Fleet, weapon_dict: Dict[id, Weapon]) -> Dict[str, int]:
@@ -18,4 +113,15 @@ def calc_final_attack(fleet: Fleet, weapon_dict: Dict[id, Weapon]) -> Dict[str, 
     -------
         dict[シチュエーション]=最終攻撃力
     """
+
+    for cl2_flg in [False, True]:
+        for formation in [Formation.LINE_AHEAD, Formation.DOUBLE_LINE,
+                          Formation.DIAMOND, Formation.ECHELON,
+                          Formation.LINE_ABREAST, Formation.FORMATION_3]:
+            for engagement in [Engagement.CROSSING_T_AD, Engagement.PARALLEL,
+                               Engagement.HEAD_ON, Engagement.CROSSING_T_DIS]:
+                for attack_type in find_attack_type(fleet.type):
+                    final_attack = calc_final_attack_impl(fleet, weapon_dict, cl2_flg, formation, engagement, attack_type)
+                    print(f'　{CL2_STR[cl2_flg]} {FORMATION_STR[formation]} {ENGAGEMENT_STR[engagement]} '
+                          f'{ATTACK_TYPE_STR[attack_type]} {final_attack}')
     return {}
