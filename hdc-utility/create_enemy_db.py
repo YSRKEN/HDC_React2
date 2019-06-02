bb2538d81d134722c29c3b340724e7762195a31d
YSRKEN <ysr.ken@gmail.com>
2019-06-02 14:21:24 +0900

    [add] 最終攻撃力をダンプする機能を実装した

--- hdc-utility/create_enemy_db.py
+++ hdc-utility/create_enemy_db.py
@@ -1,12 +1,14 @@
+import json
 import os
 import pickle
 from pprint import pprint
 from typing import List, Dict, Tuple
+from collections import OrderedDict
 
 from model.fleet import Fleet
 from model.map_position import FleetsPattern
 from model.weapon import Weapon
-from service.calc_final_attack import calc_final_attack
+from service.calc_final_attack import calc_final_attack, FORMATION_STR
 from service.crawling_map_data import get_fleets_pattern_list
 from service.crawling_weapon_data import get_enemy_weapon_list
 from service.crawling_fleet_data import get_enemy_fleet_list
@@ -69,10 +71,22 @@ def main():
         final_attack_dict[enemy_id] = calc_final_attack(enemy_data, weapon_dict)
 
     # 算出した最終攻撃力とマップの情報を編纂し、JSONとして書き出す
-    pprint(fleets_pattern_list)
-    for enemy_id in enemy_id_list:
-        print(fleet_dict[enemy_id])
-        pprint(final_attack_dict[enemy_id])
+    output_data = OrderedDict()
+    for fleets_pattern in fleets_pattern_list:
+        map_name = fleets_pattern.map_name
+        if map_name not in output_data:
+            output_data[map_name] = OrderedDict()
+        pattern_name = f'{fleets_pattern.position_name}-{fleets_pattern.pattern_index}'
+        if pattern_name not in output_data[map_name]:
+            output_data[map_name][pattern_name] = OrderedDict()
+        formation_name = FORMATION_STR[fleets_pattern.formation]
+        for enemy_id in fleets_pattern.enemy:
+            if enemy_id in output_data[map_name][pattern_name]:
+                continue
+            final_attack_data = [{'key': x[0].replace(f' {formation_name}', ''), 'val': x[1]} for x in final_attack_dict[enemy_id] if formation_name in x[0]]
+            output_data[map_name][pattern_name][enemy_id] = final_attack_data
+    with open('final_attack.json', 'w', encoding='UTF-8') as f:
+        json.dump(output_data, f, ensure_ascii=False)
 
 
 if __name__ == '__main__':
