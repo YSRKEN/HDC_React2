02e0ce4a5876f6da253294d12a141f67ed68427f
YSRKEN <ysr.ken@gmail.com>
2019-06-08 23:47:29 +0900

    [add] 比較対象を追加/削除できるようにした

--- hdc-app/src/component/KammusuSettingListView.tsx
+++ hdc-app/src/component/KammusuSettingListView.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import { IKammusuSetting } from '../constant';
-import { ListGroup } from 'react-bootstrap';
+import { ListGroup, Button } from 'react-bootstrap';
 
 const KammusuSettingListView: React.FC<{
-	kammusuSettingList: IKammusuSetting[]
-}> = ({kammusuSettingList}) => (
+	kammusuSettingList: IKammusuSetting[],
+	isAddButtonDisabled: boolean,
+	onClickAddButton: () => void,
+	onClickDeleteButton: (value: number) => void
+}> = ({kammusuSettingList, onClickAddButton, isAddButtonDisabled, onClickDeleteButton}) => (
+	<>
+	<Button className='w-100 mb-3' onClick={onClickAddButton} disabled={isAddButtonDisabled}>データを追加</Button>
 	<ListGroup>
 		{
 			kammusuSettingList.map((setting, index) => {
 				const text = `${setting.graphName}(${setting.maxHp}/${setting.armor}/${setting.nowHp})`;
+				const onclick = () => {
+					onClickDeleteButton(index);
+				}
 				return (
-					<ListGroup.Item key={index}>{text}</ListGroup.Item>
+					<ListGroup.Item key={index}>
+						{text}
+						<Button className='ml-3' onClick={onclick} key={index}>削除</Button>
+					</ListGroup.Item>
 				);
 			})
 		}
 	</ListGroup>
+	</>
 );
 
 export {KammusuSettingListView};
--- hdc-app/src/container/KammusuSettingListView.tsx
+++ hdc-app/src/container/KammusuSettingListView.tsx
@@ -6,7 +6,45 @@ import { SettingContext } from '../service/context';
 const KammusuSettingListView: React.FC = () => {
 	const setting = React.useContext(SettingContext);
 
-	return (<KSLV kammusuSettingList={setting.kammusuSettingList}/>)
+	const onClickAddButton = () => {
+		if (isAddButtonDisabled()) {
+			return;
+		}
+		setting.setKammusuSettingList([...setting.kammusuSettingList, 
+		{
+			maxHp: setting.maxHp, armor: setting.armor,
+			nowHp: setting.nowHp, graphName: setting.graphName
+		}]);
+	};
+
+	const isAddButtonDisabled = () => {
+		if (setting.kammusuSettingList.length <= 0) {
+			return false;
+		}
+		if (setting.graphName.length <= 0) {
+			return true;
+		}
+		return setting.kammusuSettingList.map(kammusu => kammusu.graphName).includes(setting.graphName);
+	};
+
+	const onClickDeleteButton = (index: number) => {
+		if (index < 0 || index >= setting.kammusuSettingList.length) {
+			return;
+		}
+		const newKammusuSettingList = [];
+		for (let i = 0; i < setting.kammusuSettingList.length; ++i) {
+			if (i == index) {
+				continue;
+			}
+			newKammusuSettingList.push(JSON.parse(JSON.stringify(setting.kammusuSettingList[i])));
+		}
+		setting.setKammusuSettingList(newKammusuSettingList);
+	};
+
+	return (<KSLV kammusuSettingList={setting.kammusuSettingList}
+		onClickAddButton={onClickAddButton}
+		isAddButtonDisabled={isAddButtonDisabled()}
+		onClickDeleteButton={onClickDeleteButton}/>)
 }
 
 export default KammusuSettingListView;
