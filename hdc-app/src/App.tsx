b2136d7ba39b8f0607d2c89d8c02c4a50d8495bd
YSRKEN <ysr.ken@gmail.com>
2021-03-02 12:49:22 +0900

    [add] stateに書き換え。それに伴いコードを更新

--- hdc-app/src/App.tsx
+++ hdc-app/src/App.tsx
@@ -4,13 +4,10 @@ import InputKammusuSetting from './container/InputKammusuSetting';
 import EnemySelector from './container/EnemySelector';
 import './App.css'
 import DamageInfo from './container/DamageInfo';
-import { SettingContext } from './service/context';
+import { SettingContext, useSettingState } from './service/context';
 import SelectButtonGroup from './container/SelectButtonGroup';
 import HeavyDamageGraph from './container/HeavyDamageGraph';
-import { IKammusuSetting } from './constant';
 import KammusuSettingListView from './container/KammusuSettingListView';
-
-type ApplicationMode = '仮想敵モード' | '大破率比較モード';
 
 const VirtualEnemy: React.FC = () => (<>
   <Col xs={12} md={10} lg={8} className='mx-auto mt-2'>
@@ -39,35 +36,16 @@ const ComparePercent: React.FC = () => (<>
 </>);
 
 const App: React.FC = () => {
-  const [maxHp, setMaxHp] = React.useState(31);
-  const [armor, setArmor] = React.useState(50);
-  const [nowHp, setNowHp] = React.useState(31);
-  const [graphName, setGraphName] = React.useState('吹雪改二');
-  const [finalAttackList, setFinalAttackList] = React.useState<{ "key": string, "val": number }[]>([]);
-  const [criticalPer, setCriticalPer] = React.useState(15);
-  const [kammusuSettingList, setKammusuSettingList] = React.useState<IKammusuSetting[]>([
-    { graphName: '神風改', maxHp: 23, armor: 38, nowHp: 23 },
-    { graphName: '睦月改二', maxHp: 27, armor: 43, nowHp: 27 },
-    { graphName: '綾波改二', maxHp: 32, armor: 54, nowHp: 32 },
-    { graphName: '暁改二', maxHp: 31, armor: 50, nowHp: 31 },
-  ]);
-  const [maxValue, setMaxValue] = React.useState(0);
-  const [minValue, setMinValue] = React.useState(200);
-  const [applicationMode, setApplicationMode] = React.useState<ApplicationMode>('大破率比較モード');
+  const state = useSettingState();
 
   const setApplicationModeFunc = (value: string) => {
     if (['仮想敵モード', '大破率比較モード'].includes(value)) {
-      setApplicationMode(value as ApplicationMode);
+      state.dispatch({ type: 'setApplicationMode', message: value });
     }
   }
 
   return (
-    <SettingContext.Provider value={{
-      maxHp, setMaxHp, armor, setArmor, nowHp, setNowHp, graphName, setGraphName,
-      finalAttackList, setFinalAttackList, criticalPer, setCriticalPer,
-      kammusuSettingList, setKammusuSettingList, maxValue, setMaxValue,
-      minValue, setMinValue
-    }}>
+    <SettingContext.Provider value={state}>
       <Container className='my-3'>
         <Row>
           <Col xs={12} md={10} lg={8} className='mx-auto'>
@@ -90,16 +68,16 @@ const App: React.FC = () => {
           </Col>
           <Col xs={12} md={10} lg={8} className='mx-auto mt-2'>
             <SelectButtonGroup className='w-100 my-3' nameList={['仮想敵モード', '大破率比較モード']}
-              firstSelectName={applicationMode} callback={setApplicationModeFunc} />
+              firstSelectName={state.applicationMode} callback={setApplicationModeFunc} />
           </Col>
         </Row>
         <Row>
           {
-            applicationMode === '仮想敵モード' ? <VirtualEnemy /> : <ComparePercent />
+            state.applicationMode === '仮想敵モード' ? <VirtualEnemy /> : <ComparePercent />
           }
         </Row>
       </Container>
-    </SettingContext.Provider>
+    </SettingContext.Provider >
   );
 }
 
--- hdc-app/src/container/KammusuSettingListView.tsx
+++ hdc-app/src/container/KammusuSettingListView.tsx
@@ -1,50 +1,33 @@
 import * as React from 'react';
 
-import {KammusuSettingListView as KSLV} from '../component/KammusuSettingListView';
+import { KammusuSettingListView as KSLV } from '../component/KammusuSettingListView';
 import { SettingContext } from '../service/context';
 
 const KammusuSettingListView: React.FC = () => {
-	const setting = React.useContext(SettingContext);
+	const { kammusuSettingList, maxHp, armor, nowHp, graphName, dispatch } = React.useContext(SettingContext);
 
 	const onClickAddButton = () => {
-		if (isAddButtonDisabled()) {
-			return;
-		}
-		setting.setKammusuSettingList([...setting.kammusuSettingList, 
-		{
-			maxHp: setting.maxHp, armor: setting.armor,
-			nowHp: setting.nowHp, graphName: setting.graphName
-		}]);
+		dispatch({ type: 'addKammusuSetting' });
 	};
 
 	const isAddButtonDisabled = () => {
-		if (setting.kammusuSettingList.length <= 0) {
+		if (kammusuSettingList.length <= 0) {
 			return false;
 		}
-		if (setting.graphName.length <= 0) {
+		if (graphName.length <= 0) {
 			return true;
 		}
-		return setting.kammusuSettingList.map(kammusu => kammusu.graphName).includes(setting.graphName);
+		return kammusuSettingList.map(kammusu => kammusu.graphName).includes(graphName);
 	};
 
 	const onClickDeleteButton = (index: number) => {
-		if (index < 0 || index >= setting.kammusuSettingList.length) {
-			return;
-		}
-		const newKammusuSettingList = [];
-		for (let i = 0; i < setting.kammusuSettingList.length; ++i) {
-			if (i === index) {
-				continue;
-			}
-			newKammusuSettingList.push(JSON.parse(JSON.stringify(setting.kammusuSettingList[i])));
-		}
-		setting.setKammusuSettingList(newKammusuSettingList);
+		dispatch({ type: 'deleteKammusuSetting', message: '' + index });
 	};
 
-	return (<KSLV kammusuSettingList={setting.kammusuSettingList}
+	return (<KSLV kammusuSettingList={kammusuSettingList}
 		onClickAddButton={onClickAddButton}
 		isAddButtonDisabled={isAddButtonDisabled()}
-		onClickDeleteButton={onClickDeleteButton}/>)
+		onClickDeleteButton={onClickDeleteButton} />)
 }
 
 export default KammusuSettingListView;
